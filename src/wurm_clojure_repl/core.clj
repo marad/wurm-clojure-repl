f76431da9e1572e31aa31b1ddfcf209fb417e1ad
Marcin Radoszewski <moriturius@gmail.com>
2016-12-30 20:20:04 +0000

    Mod properties and code cleanup

--- src/wurm_clojure_repl/core.clj
+++ src/wurm_clojure_repl/core.clj
@@ -1,5 +1,5 @@
 (ns wurm-clojure-repl.core
-  (:require [clojure.tools.nrepl.server :refer [start-server]]
+  (:require [clojure.tools.nrepl.server :refer [start-server stop-server]]
             wurm)
   (:import [org.gotti.wurmunlimited.modloader.interfaces WurmClientMod Initable]
            [org.gotti.wurmunlimited.modloader.classhooks HookManager]
@@ -15,15 +15,26 @@
            (wurm/init-ns WurmClientBase))))
 
 (defn -init [this]
-  (def server (start-server :port 7888))
-  (.registerHook (HookManager/getInstance)
-                 "com.wurmonline.client.renderer.gui.HeadsUpDisplay"
-                 "init"
-                 "(II)V"
-                 (reify java.lang.reflect.InvocationHandler
-                   (invoke [this proxy method args]
-                     (.invoke method proxy args)
-                     (println ">>>> Defining usefull stuff")
-                     (define-wurm-namespace)
-                     nil))))
+  (let [server (start-server :port 7888)]
+
+    (.registerHook (HookManager/getInstance)
+                   "com.wurmonline.client.renderer.gui.HeadsUpDisplay"
+                   "init"
+                   "(II)V"
+                   (reify java.lang.reflect.InvocationHandler
+                     (invoke [this proxy method args]
+                       (.invoke method proxy args)
+                       (println ">>>> Defining usefull stuff")
+                       (define-wurm-namespace)
+                       nil)))
+
+    (.registerHook (HookManager/getInstance)
+                   "com.wurmonline.client.WurmClientBase"
+                   "performShutdown"
+                   "()V"
+                   (reify java.lang.reflect.InvocationHandler
+                     (invoke [this proxy method args]
+                       (stop-server server)
+                       (.invoke method proxy args)
+                       nil)))))
 
